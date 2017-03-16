030065d9e09526002eb7476c54361819899328ea
yapetrichka <yapetrichka@google.com>
2017-03-16 12:44:50 +0300

    Core files

---
+++ SmartExporter/Editor/Menu/SmartExporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SmartExporter
+{
+    #region PRIVATE_VARIABLES
+
+    private const string menuTitle = "Assets/Playflock/SmartExport";
+
+    #endregion
+
+    [MenuItem(menuTitle)]
+    static void Export()
+    {
+        var selectedObjs = Selection.objects;
+        var assetPathNames = new string[selectedObjs.Length];
+        for ( var i = 0; i < selectedObjs.Length; i++ )
+        {
+            assetPathNames[i] = AssetDatabase.GetAssetPath(selectedObjs[i]);
+        }
+        var packageName = "packageName";
+        if ( selectedObjs.Length == 1 )
+            packageName = selectedObjs[0].name;
+        var savePath = EditorUtility.SaveFilePanel("Save package to folder", "", packageName, "unitypackage");
+        if(string.IsNullOrEmpty( savePath ))
+            return;
+        AssetDatabase.ExportPackage( assetPathNames, savePath, ExportPackageOptions.IncludeDependencies );
+        EditorUtility.DisplayDialog( "Export done", "To path : " + savePath, "OK" );
+    }
+}
